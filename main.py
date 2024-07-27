6bee9def20174c003a524df757a0595ada31b1ad
FazalKadivar21 <fazalkadivar7@gmail.com>
2024-07-27 11:35:23 +0530

    first commit

--- main.py
+++ main.py
@@ -4,3 +4,23 @@ import os
 import random
 import numpy as np
 from matplotlib import pyplot as plt
+
+# Import tensorflow dependencies - Functional API
+from keras import Model
+from keras import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
+import tensorflow as tf
+
+# Avoid OOM errors by setting GPU Memory Consumption Growth
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# for gpu in gpus: 
+#     tf.config.experimental.set_memory_growth(gpu, True)
+
+# Setup paths
+POS_PATH = os.path.join('data', 'positive')
+NEG_PATH = os.path.join('data', 'negative')
+ANC_PATH = os.path.join('data', 'anchor')
+
+# Make the directories
+os.makedirs(POS_PATH)
+os.makedirs(NEG_PATH)
+os.makedirs(ANC_PATH)
